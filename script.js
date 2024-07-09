c656726f750b015a248e7d334a7240ed90085748
Amechi <amechsele@gmail.com>
2024-07-08 19:21:22 -0600

    latest edit

---
+++ script.js
@@ -0,0 +1,45 @@
+let container = document.getElementById("container");
+let select = document.getElementById("select");
+let reset = document.getElementById("reset");
+
+createGrid(16);
+
+select.addEventListener('click', () => {
+    resetGrid();
+    let num = parseInt(prompt("enter a number between 1 and 100 included"));
+    while(isNaN(num) || num > 100 || num < 0){
+        alert("Input error");
+        num = parseInt(prompt("Enter a number between 1 and 100 included"));
+    }
+       createGrid(num);});
+
+function createGrid(size){
+    for(let i=0; i < size; i++){
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for(let j=0; j < size; j++){
+            let cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            cell.addEventListener('mouseover', (e) => e.target.style.backgroundColor = randomColor());
+            row.appendChild(cell);
+
+        
+    }
+}
+}
+
+function randomColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    let color = "rgb(" + r + "," + g + "," + b + ")";
+    return color;
+};
+
+function resetGrid(){
+    container.innerHTML = '';
+}
+
+reset.addEventListener('click', () => resetGrid());
